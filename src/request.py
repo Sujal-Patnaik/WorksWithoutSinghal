a03a7f1829687e45e1a4843bafec9ce6330f50aa
Sujal-Patnaik <sujalpatnaik5@gmail.com>
2026-03-31 16:35:05 +0530

    Update request.py

--- src/request.py
+++ src/request.py
@@ -1,1 +1,21 @@
+class Node:
+  def __init__(self, node_id, x, y, demand, TW_Early, TW_Latest, service_time):
+    self.node_id = node_id
+    self.x = x
+    self.y = y
+    self.demand = demand
+    self.TW_Early = TW_Early
+    self.TW_Latest = TW_Latest
+    self.service_time = service_time
 
+class Request:
+  def __init__(self, request_id, pickup, delivery, feasible_fleet=None):
+    self.request_id = request_id
+    self.pickup = pickup
+    self.delivery = delivery
+    self.feasible_fleet = feasible_fleet
+  def __deepcopy__(self,memo):
+    new_request = Request(request_id = self.request_id, pickup = self.pickup, delivery = self.delivery,
+                          feasible_fleet = copy.deepcopy(self.feasible_vehicles,memo))
+    memo[id(self)] = new_request
+    return new request
