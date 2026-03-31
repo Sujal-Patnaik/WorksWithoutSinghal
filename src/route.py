707c1402f5e0d3dff7caad63ed640f54831e3b80
Sujal-Patnaik <sujalpatnaik5@gmail.com>
2026-03-31 16:54:34 +0530

    Update route.py

--- src/route.py
+++ src/route.py
@@ -1,1 +1,10 @@
+import copy
+from src.request import Node
+from src.request import Request
 
+class Vehicle:
+  def __init__(self, vehicle_id, capacity, start_node_id, end_node_id):
+    self.vehicle_id = vehicle_id
+    self.capacity = capacity
+    self.start_node_id = start_node_id
+    self.end_node_id = end_node_id
