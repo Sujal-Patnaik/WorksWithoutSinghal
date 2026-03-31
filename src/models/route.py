73055f7cea16420cbf77bd05c183bbc95c5a3fb4
sudipto818 <sudiptoghosh096@gmail.com>
2026-03-31 17:08:15 +0530

    Update route.py

--- src/models/route.py
+++ src/models/route.py
@@ -8,3 +8,18 @@ class Vehicle:
     self.capacity = capacity
     self.start_node_id = start_node_id
     self.end_node_id = end_node_id
+
+class ProblemData:
+    def __init__(self, nodes, requests, vehicles):
+        self.nodes = {n.node_id: n for n in nodes}
+        self.requests = {r.request_id: r for r in requests}
+        self.vehicles = {v.vehicle_id: v for v in vehicles}
+
+class Route:
+    def __init__(self, vehicle_id: int, problem_data: ProblemData):
+        self.vehicle_id = vehicle_id
+        vehicle = problem_data.vehicles[vehicle_id]
+        self.visits = [vehicle.start_node_id, vehicle.end_node_id]
+        self.assigned_requests = set()
+        self.visits = [vehicle.start_node_id, vehicle.end_node_id]
+        self.assigned_requests = set()
